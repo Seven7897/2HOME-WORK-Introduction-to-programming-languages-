67c512b2a2c42b28859baafba67d0dcdda22af9c
Anatoly Finchenko <finchenko777@gmail.com>
2022-06-26 20:17:58 +0300

    *Первое_задание_второго_ДЗ*

---
+++ homework001/Program.cs
@@ -0,0 +1,6 @@
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+*/
+Console.WriteLine("Введите трех значное число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number / 10 ;
+Console.WriteLine( result % 10  );
